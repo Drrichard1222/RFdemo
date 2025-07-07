c497d27fac890168aa996bad1192034837acf139
Drrichard1222 <drrichard1701@gmail.com>
2025-07-07 21:54:25 +0800

    Add files via upload

--- predictor.py
+++ predictor.py
@@ -19,53 +19,58 @@ import matplotlib. pyplot as plt
 #从LIME 库中导入LimeTabuLarExpLainer,用于解释表格数据的机器学习模型
 from lime.lime_tabular import LimeTabularExplainer
 
-#加载训练好的随机森林模型（RF.pkL）
-model = joblib.load('RF.pkl')
+#加载训练好的模型（LRR.pkL）
+model = joblib.load('LRR.pkl')
 
 #从X_test.csv文件加载测试数据，以便用于LIME解释器
 X_test = pd.read_csv('X_test.csv')
 
 #定义特征名称，对应数据集中的列名
 feature_names = [
-    "Antibiotic",
-    "Ventilation",
-    "Congestive_heart_failure",
-    "Cerebrovascular_disease",
-    "Chronic_pulmonary_disease", 
-    "Paraplegia",
-    "Respiratory_rate_min", 
-    "Temperature_max"
+    "Sodium_max", # Sodium_max
+    "Vasopressor", # Vasopressor
+    "APSIII", # APSIII
+    "Age", # Age
+    "Calcium_max", # Calcium_max
+    "Respiratory_rate_min", # Respiratory_rate_min
+    "Ventilation", # Ventilation
+    "Dementia", # Dementia
+    "Metastatic_solid_tumor", # Metastatic_solid_tumor
 ]
 
 # Streamlit用面
-st.title("TBI患者肺炎发生风险预测器") # 设置网页标题
+st.title("In-hospital mortality risk of TBI patients") # 设置网页标题
          
-# Antibiotic：数值输入框
-Antibiotic = st.selectbox("Antibiotic:", options=[0, 1], format_func=lambda x:"0"if x==1 else"1")
+# Sodium_max：数值输入框
+Sodium_max = st.number_input("Sodium_max:", min_value=0, max_value=500, value=12)
+
+# Vasopressor：数值输入框
+Vasopressor = st.selectbox("Vasopressor:", options=[0, 1], format_func=lambda x:"0"if x==1 else"1")
+
+# APSIII：数值输入框
+APSIII = st.number_input("APSIII:", min_value=0, max_value=100, value=10)
+
+# Age：数值输入框
+Age = st.number_input("Age:", min_value=0, max_value=100, value=65)
+
+# Calcium_max：数值输入框
+Calcium_max = st.number_input("Calcium_max:", min_value=0, max_value=20, value=20)
+
+# Respiratory_rate_min：数值输入框
+Respiratory_rate_min = st.number_input("Respiratory_rate_min:", min_value=0, max_value=30, value=20)
 
 # Ventilation：数值输入框
 Ventilation = st.selectbox("Ventilation:", options=[0, 1], format_func=lambda x:"0"if x==1 else"1")
 
-# Congestive_heart_failure：数值输入框
-Congestive_heart_failure = st.selectbox("Congestive_heart_failure:", options=[0, 1], format_func=lambda x:"0"if x==1 else"1")
+# Dementia：数值输入框
+Dementia = st.selectbox("Dementia:", options=[0, 1], format_func=lambda x:"0"if x==1 else"1")
 
-# Cerebrovascular_disease：数值输入框
-Cerebrovascular_disease = st.selectbox("Cerebrovascular_disease:", options=[0, 1], format_func=lambda x:"0"if x==1 else"1")
-
-# Chronic_pulmonary_disease：数值输入框
-Chronic_pulmonary_disease = st.selectbox("Chronic_pulmonary_disease:", options=[0, 1], format_func=lambda x:"0"if x==1 else"1")
-
-# Paraplegia：数值输入框
-Paraplegia = st.selectbox("Paraplegia:", options=[0, 1], format_func=lambda x:"0"if x==1 else"1")
-
-# Respiratory_rate_min：数值输入框
-Respiratory_rate_min = st.number_input("Respiratory_rate_min:", min_value=0, max_value=35, value=18)
-
-# Temperature_max：数值输入框
-Temperature_max = st.number_input("Temperature_max:", min_value=0, max_value=50, value=36)
+# Metastatic_solid_tumor：数值输入框
+Metastatic_solid_tumor = st.selectbox("Metastatic_solid_tumor:", options=[0, 1], format_func=lambda x:"0"if x==1 else"1")
 
 # 处理输入数据并进行预测
-feature_values = [Antibiotic, Ventilation, Congestive_heart_failure, Cerebrovascular_disease, Chronic_pulmonary_disease, Paraplegia, Respiratory_rate_min,Temperature_max]# 将用户输入的特征值存入列表
+feature_values = [Sodium_max, Vasopressor, APSIII, Age, Calcium_max, Respiratory_rate_min, Ventilation, Dementia, Metastatic_solid_tumor]# 将用户输入的特征值存入列表
+
 features=np.array([feature_values])# 将特征转换为NumPy 数组，适用于模型输入
 
 # 当用户点击“ Predict”按钮时执行以下代码
@@ -77,7 +82,7 @@ if st.button("Predict"):
 
 
     # 显示预测结果
-    st.write(f"**Predicted Class: * {predicted_class} (1: Disease, 0: No Disease)")
+    st.write(f"**Predicted Class: * {predicted_class} (1: high risk of in-hospital mortality, 0: low risk of in-hospital mortality)")
     st.write(f"**Prediction Probabilities: ** {predicted_proba}")
 
     # 根据预测结果生成建议
@@ -85,15 +90,15 @@ if st.button("Predict"):
     # 如果预测类别为1（高风险）
     if predicted_class == 1:
         advice = (
-            f"According to our model, you have a high risk of XX disease. "
-            f"The model predicts that your probability of having XX disease is {probability:.1f}%. "
+            f"According to our model, you have a high risk of in-hospital mortality. "
+            f"The model predicts that your probability of having risk of in-hospital mortality is {probability:.1f}%. "
             "It's advised to consult with your healthcare provider for further evaluation and possible intervention."
         )
     # 如果预测类别为0（低风险）
     else:
         advice = (
-            f"According to our model, you have a low risk of XX disease."
-            f"The model predicts that your probability of not having XX disease is {probability:.1f}%."
+            f"According to our model, you have a low risk of in-hospital mortality."
+            f"The model predicts that your probability of not risk of in-hospital mortality is {probability:.1f}%."
             "However, maintaining a healthy lifestyle is important. Please continue regular check-ups with your healthcare provider."
         )
     # 显示建议
@@ -102,7 +107,7 @@ if st.button("Predict"):
     # SHAP解释
     st.subheader("SHAP Force Plot Explanation") 
     #创建SHAP解释器，基于树模型（如随机森林）
-    explainer_shap = shap.LinearExplainer(model) 
+    explainer_shap = shap.KernelExplainerr(model) 
     #计算SHAP值，用于解释模型的预测
     shap_values = explainer_shap. shap_values(pd.DataFrame([feature_values], columns=feature_names)) 
     
@@ -128,7 +133,7 @@ if st.button("Predict"):
     lime_explainer = LimeTabularExplainer(
         training_data=X_test.values,
         feature_names=X_test.columns.tolist(),
-        class_names=['Not sick', 'Sick'], # Adjust class names to match your classification task mode='classification
+        class_names=['Low risk', 'High risk'], # Adjust class names to match your classification task mode='classification
         mode='classification'
 )
 
